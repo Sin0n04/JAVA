8c7099ab6eb0676d5d47cb7e3bb84842aa3b796e
Sin0n-emmanuel <emmanuelani1119@gmail.com>
2025-11-03 09:10:26 +0100

    class work 3/11/25

--- src/exercises/EX2_5.java
+++ src/exercises/EX2_5.java
@@ -2,27 +2,37 @@ package exercises;
 
 public class EX2_5 {
     public static void main(String[] args){
-        int n,digit,num;
+        int base,digit,num,decimal;
         boolean is_binary = true;
 
 
         System.out.println("input integer to transform to binary: ");
-        n = Console.readInt();
+        num = Console.readInt();
 
-        num = n;
+        base = 1 ;
+        decimal = 0;
 
-        while(num > 0){
+        while (num > 0 && is_binary){
+
             digit = num % 10;
 
-            if(digit > 1){
+            if (digit != 1 && digit != 0){
                 is_binary = false;
             }
+
+            decimal += (base*digit);
+            base *= 2;
             num /= 10;
         }
 
         if (is_binary){
-            String binaryString = Integer.toBinaryString(n);
-            System.out.println("Binary Value: " + binaryString);
+            System.out.println("decimal equivalent: " + decimal);
         }
+        else{
+            System.out.println("Not a Binary number");
+        }
+
+
+
     }
 }
