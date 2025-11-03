db26ec38eb94b8d146a334612fb8f30f453fef97
Sin0n-emmanuel <emmanuelani1119@gmail.com>
2025-11-03 10:51:23 +0100

    class work 3/11/25

---
+++ src/exercises/EX3_2.java
@@ -0,0 +1,34 @@
+package exercises;
+
+public class EX3_2 {
+    public static void main(String[] args){
+        int amount,c,rate,years,n,count;
+        double r,power,payment,total;
+
+        System.out.println("input amount to be financed: ");
+        amount = Console.readInt();
+
+        System.out.println("input anual interest rate(0-100): ");
+        rate = Console.readInt();
+
+        System.out.println("input number of years to repay loan: ");
+        years = Console.readInt();
+
+        power = 1;
+        n = years * 12;
+        c = amount;
+        r = (double) rate / 100;
+        count = 1;
+
+        while(count <= n){
+            power *= (1/(1+r));
+            count++;
+        }
+
+        payment = ((c*r)/(1-power));
+        total = payment * n;
+
+        System.out.println("if we borrow " + amount + " at " +rate+ "% interest to be paid in " + years+ " years, the monthly payment would be " + payment +" and the total payment: " + total  );
+
+    }
+}
