8566808df2620c40f39c12c43d88d3f108e68934
Sin0n-emmanuel <emmanuelani1119@gmail.com>
2025-11-03 09:10:26 +0100

    class work 3/11/25

---
+++ src/exercises/EX2_10.java
@@ -0,0 +1,9 @@
+package exercises;
+
+public class EX2_10 {
+    public static void main(String[] args){
+
+
+
+    }
+}
---
+++ src/exercises/EX3_1.java
@@ -0,0 +1,16 @@
+package exercises;
+
+public class EX3_1 {
+    public static void main(String[] args){
+
+        int n,num,avg,sumprime;
+
+        System.out.println("input starting number: ");
+        num = Console.readInt();
+        System.out.println("input ammount of prime numbers to display: ");
+        n = Console.readInt();
+
+
+
+    }
+}
