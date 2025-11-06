d9b523f15e2610270c768544a5dac5a4b2845965
Sin0n04 <emmanuelani1119@gmail.com>
2025-11-06 17:18:44 +0100

    exam exercise

---
+++ src/exercises/EX3_4.java
@@ -0,0 +1,13 @@
+package exercises;
+
+public class EX3_4 {
+    public static void main(String[] args){
+        int n_ini,nfinal;
+
+        System.out.println("input number: ");
+        n_ini = Console.readInt();
+
+
+    }
+
+}
