2098282d717ef9b6c1ddd624604619a1d07ee459
Sin0n04 <emmanuelani1119@gmail.com>
2025-11-15 09:51:57 +0100

    Homework done on 15/11/2025

--- src/packhouses/House.java
+++ src/packhouses/House.java
@@ -1,7 +1,7 @@
 package packhouses;
 
 public class House {
-    int area;
+    double area;
     String address;
     char type;
 
@@ -23,7 +23,7 @@ public class House {
         if (this.area <= 60){
             return tax;
         }
-        if(this.area > 60 && this.area < 80){
+        if(this.area < 80){
             tax = this.area*4.5;
             return tax;
         }
@@ -38,6 +38,59 @@ public class House {
             return true;
         }
         return (this.type == 'f' && this.area >100);
+    }
+
+    boolean isSimilar(House other){
+        return (this.type == other.type&& (this.area- other.area >(-10)&& this.area- other.area < 10));
+
+    }
+
+    public static void main(String[] args){
+        House h1,h2,h3;
+
+        h1 = new House();
+        h1.area = 65;
+        h1.type = 'f';
+        h1.address = " C/ Rioja, 18 ";
+
+        h2 = new House();
+        h2.address = " C/ Alava, 5 ";
+        h2.area = 150;
+        h2.type = 'd';
+
+        h3 = new House();
+        h3.address = " C/ Gorbea, 23 ";
+        h3.area = 105.5;
+        h3.type = 'f';
+
+        h1.show();
+        h2.show();
+        h3.show();
+
+        h2.area -= 5;
+
+
+        h1.show();
+        h1.giveMeTax();
+        h2.show();
+        h2.giveMeTax();
+        h3.show();
+        h3.giveMeTax();
+
+        System.out.println("is house 1 able to be sold?: " + h1.canBeSold());
+        System.out.println("is house 2 able to be sold?: " + h2.canBeSold());
+        System.out.println("is house 3 able to be sold?: " + h3.canBeSold());
+
+        System.out.println(" is h2 similar to h3?: " + h2.isSimilar(h3));
+        System.out.println(" is h1 similar to h3?: " + h1.isSimilar(h3));
+
+        h1.area += 35;
+
+        System.out.println("is H1 similar to h3 now?: " + h1.isSimilar(h3));
+
+
+
+
     }
 
 
