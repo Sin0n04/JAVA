da27ba071baee7b50a0b0d99a6ccdcb997c272c0
Sin0n04 <emmanuelani1119@gmail.com>
2025-11-24 13:56:09 +0100

    Homework done on 24/11/2025

--- src/ex_3/StaticLibrary.java
+++ src/ex_3/StaticLibrary.java
@@ -34,10 +34,39 @@ public class StaticLibrary {
         return  x;
     }
 
+    static int summation(int x){
+        int sum = 0;
+        for (int count = 1 ; count < x;count ++){
+            sum += count;
+
+        }
+        return sum;
+
+    }
+
+    static void showTable(int x){
+        for (int count = 1 ; count <= 10 ; count ++){
+            System.out.println(x + " * " + count + " = " + count * x );
+        }
+    }
+
+    static boolean showRestrictedTable(int x){
+
+        if(x >= 1 && x <= 20){
+            for (int count = 1 ; count <= 10 ; count ++){
+                System.out.println(x + " * " + count + " = " + count * x );
+            }
+            return true;
+        }
+        System.out.println("Not able to show multiplication table.....");
+        return false;
+
+    }
+
 
 	
 	public static void main(String[] args) {
-		int n1 = 1,n2 = 1, base;
+		int n1 = 1,n2 = 1, base,x;
         char input;
 
         while(n1 != 0 && n2 != 0){
@@ -72,6 +101,34 @@ public class StaticLibrary {
         for(int count = 1 ; count <= base ; count++){
             System.out.println(giveMePower(7,count));
         }
+
+
+        x = Console.readInt();
+        double summation = 0;
+        for(int count = 1 ; count <= 8 ; count ++){
+            summation = summation + (StaticLibrary.giveMePower(x,count)/StaticLibrary.summation(count));
+
+        }
+
+        System.out.println("Input number to show multiplication table: ");
+        x = Console.readInt();
+
+        while(x > 10 || x < 0 ){
+            System.out.println("Wrong base, input a number between 1-10");
+            x = Console.readInt();
+        }
+        StaticLibrary.showTable(x);
+
+
+        x = 1;
+        while(x != 0){
+            System.out.println("Input integer to show restricted table: ");
+            x = Console.readInt();
+            StaticLibrary.showRestrictedTable(x);
+        }
+
+
+
 
 
 
