a7176dba2c0a04cbcdc6e57733df8758a6e1cb4a
Sin0n04 <emmanuelani1119@gmail.com>
2025-11-24 14:01:38 +0100

    Homework half done on 24/11/2025

---
+++ src/words/Palindromic.java
@@ -0,0 +1,17 @@
+package words;
+
+public class Palindromic {
+    long num;
+
+    Palindromic(){
+
+    }
+
+    Palindromic(long n){
+        this.num = n;
+    }
+
+    boolean isPalindrome(){
+        return true;
+    }
+}
