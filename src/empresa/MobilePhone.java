d86f43cf94b67727bf89cff6273d8e1fcf7b9fa2
dw1 <emmanuelani1119@gmail.com>
2025-12-09 12:24:49 +0100

    Work half done on 09/12/2025

---
+++ src/empresa/MobilePhone.java
@@ -0,0 +1,25 @@
+package empresa;
+
+public class MobilePhone {
+    private String number;
+    private int battery;
+
+
+    public MobilePhone(String number, int battery){
+        this.battery = battery;
+        this.number = number;
+    }
+
+    public void use(){
+        if(battery > 0 ){
+            battery -= 1;
+        }
+        else{
+            System.out.println("Phone is dead, Please charge....");
+        }
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
---
+++ src/empresa/Worker.java
@@ -0,0 +1,34 @@
+package empresa;
+
+public class Worker extends Person{
+    private int money;
+    private MobilePhone phone;
+
+    public Worker(String dni, String name, int height, int weight,int money, MobilePhone phone){
+        super(dni,name,height,weight);
+        this.money = money;
+        this.phone = phone;
+    }
+
+    void earnMoney(int ammount){
+        this.money += ammount;
+    }
+
+    void spendMoney(int ammount){
+        this.money -= ammount;
+    }
+
+    @Override
+    public String  toString(){
+        return "name: " + this.getName() + " weight: " + this.getWeight() + " money: " + this.money;
+    }
+
+    public int getMoney(){
+        return money;
+    }
+
+    public void work(){
+        this.phone.use();
+        this.earnMoney(10);
+    }
+}
