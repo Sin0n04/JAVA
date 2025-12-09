7022e2922e25552a0875b736aab12434041d634f
dw1 <emmanuelani1119@gmail.com>
2025-12-09 10:37:09 +0100

    work 09/12/2025

--- src/city/Apartment.java
+++ src/city/Apartment.java
@@ -1,5 +1,31 @@
 package city;
 
-public class Apartment {
+public class Apartment extends House {
+    private int floor;
+    private boolean has_elevator;
+
+
+    Apartment(int y, double a, String add, int floor, boolean has_elevator){
+        super(y,a,add);
+        this.floor = floor;
+        this.has_elevator = has_elevator;
+    }
+
+    Apartment(){
+
+    }
+
+    void view(){
+        System.out.println("Address: "+this.address+ " floor:" +  this.floor + " area: "this.area + " Has elevator: ",this.has_elevator+ " year of contruction: " + this.yearOfConstruction);
+    }
+
+    Apartment compareApartments(Apartment other){
+
+    }
+
+
+
+
+
 
 }
