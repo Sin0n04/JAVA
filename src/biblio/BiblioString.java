3aa9af2dd2af4c4fc61ff14d6a55c61434594dfa
Sin0n04 <emmanuelani1119@gmail.com>
2025-12-22 15:01:22 +0100

    work done on 22/12/2025

---
+++ src/biblio/BiblioString.java
@@ -0,0 +1,68 @@
+package biblio;
+
+public class BiblioString {
+
+    protected static String reverseConcatenate(){
+        String str = "";
+        String toreverse = "";
+        for (int count = 1; count <= 10 ; count ++){
+            str = Console.readString() ;
+            if (str.equals("end")){
+                break;
+            }
+            else{
+                toreverse += str + " ";
+            }
+        }
+        String Reversed = "";
+        int end = toreverse.length() -1;
+        for (int i = toreverse.length()-1 ; i >= 0 ;i-- ){
+            if(toreverse.charAt(i) == ' '){
+                Reversed += toreverse.substring(i + 1, end + 1 ) + " ";
+                end = i - 1;
+            }
+
+        }
+        return Reversed.trim();
+
+
+    }
+
+    protected static int replace(String str, char ocurrence, char substitute){
+        int replacecounter = 0;
+        for (int i = 0 ; i < str.length(); i ++){
+            if (str.charAt(i) == ocurrence){
+                str = str.substring(0,i) + substitute + str.substring(i+1);
+                replacecounter++;
+            }
+
+        }
+        return replacecounter;
+
+    }
+
+    protected static String removeChar(String str, char occurence){
+        String result = "";
+        for(int i = 0; i < str.length(); i ++){
+            if (str.charAt(i) != occurence){
+                result += str.charAt(i);
+            }
+        }
+        return result;
+    }
+
+    protected static String completeWith$(String str,int length){
+        String result = str;
+        for(int i = str.length(); i < length; i++){
+            result += "$";
+        }
+        return result;
+    }
+
+    protected static String removeWord(String str1, String str2){
+        return str1;
+    }
+
+
+
+}
