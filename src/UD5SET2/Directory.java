523bd7916f6ecec31bc07be43bfa72b4c2753e35
Sin0n04 <emmanuelani1119@gmail.com>
2025-12-22 22:43:25 +0100

    work done on 22/12/2025

--- src/UD5SET2/Directory.java
+++ src/UD5SET2/Directory.java
@@ -3,4 +3,38 @@ package UD5SET2;
 public class Directory {
     private String name;
     private int size;
+    private final int MAXCHAR = 40;
+
+    private Directory(String name , int size){
+        this.name = name;
+        this.size = size;
+    }
+
+    private void view(){
+        System.out.println("name of the directory: " + this.name + " , size of the directory in bytes: " + this.size);
+    }
+
+    private int levels() {
+        int levels = 0;
+        for(int i = 0; i < this.name.length();i++){
+            if(this.name.charAt(i)== '/'){
+                levels++;
+            }
+        }
+        return levels + 1;
+    }
+
+    private String lastLevel(){
+        String str = this.name;
+        for(int i = str.length() - 1 ; i >= 0 ; i-- ){
+            if(str.charAt(i) == '/'){
+                str = str.substring(i+1);
+                break;
+            }
+        }
+        return str;
+    }
+
+
+
 }
