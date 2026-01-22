632b800cf7af6717027ddefd3ac3552cb0e9e84c
Sin0n04 <emmanuelani1119@gmail.com>
2026-01-22 09:09:50 +0100

    Update RectangleManager.java

--- src/UD6SET1/RectangleManager.java
+++ src/UD6SET1/RectangleManager.java
@@ -35,5 +35,43 @@ public class RectangleManager {
         }
     }
 
+			
+
+		public int howManyFitInside(Rectangle r1){
+		
+			double area1 = r1.areaR();
+			int count = 0;
+		
+			for(int i = 0 ; I < this.arr.length() ; i++){
+
+					if(area1 >= arr[i].areaR)
+							count++;
+			}
+			return count;
+		}
+
     //exercise half done...
+
+
+		 Public Rectangle[] fusionArray(RectangleManager other){
+
+				Rectangle[] fusion = new Rectangle[this.arr.length() + other.arr.length()];
+				int z = 0;
+				int j = 0;
+				
+				for(int i = 0 ; i < fusion.length() ; i++){
+					
+						if(i % 2 == 0 && j < other.arr.length()){
+								fusion[i] = other.arr[j];
+								j++;
+						}
+						if(i % 2 != 0 && this.arr.length > z){
+								fusion[i] = this.arr[z];
+								z++;
+						}
+				} 
+				return fusion;
+
+			}
 }
+ 
