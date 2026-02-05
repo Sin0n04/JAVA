c76540b69a4c7624c1373d8aacdcb9992fcd959b
Sin0n04 <emmanuelani1119@gmail.com>
2026-02-05 20:22:09 +0100

    work done on 03/02/2026 - afternoon

--- src/UD6SET2/Matrix1.java
+++ src/UD6SET2/Matrix1.java
@@ -13,12 +13,53 @@ public class Matrix1 {
         if(row > NR || col > NC){
             System.out.println("inputted values exceed the maximum capacity, instantiating with maximum values....");
             this.arr = new int[NR][NC];
+            loadByRows(this.arr);
         }
-        else
+        else {
             this.arr = new int[row][col];
+            loadByRows(this.arr);
+        }
+
     }
 
-    protected void loadByRows(){
+    protected static void loadByRows(int[][] arr){
+        int max=10, min=1;
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = 0; j < arr[i].length ; j++) {
+                int random = min +(int)(Math.random()*(max-min+1));
+                arr[i][j] = random;
+            }
+
+           max+=10;
+           min+=10;;
+        }
+
+    }
+
+    public void view(){
+        for (int i = 0; i < this.arr.length; i++) {
+            for (int j = 0; j < this.arr[0].length; j++) {
+                System.out.print(this.arr[i][j] + " | ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void reverseRows(){
+        for (int i = 0; i < this.arr.length; i++) {
+            int[] reversed = new int[this.arr[0].length];
+            int end =this.arr[0].length - 1;
+            for (int j = 0; j < this.arr[0].length; j++) {
+                reversed[end] = arr[i][j];
+                end--;
+            }
+            this.arr[i] = reversed;
+        }
+
+    }
+
+    public void swapRows(int row1, int row2){
 
     }
 }
---
+++ src/UD6SET2/Test_Matrix1.java
@@ -0,0 +1,7 @@
+package UD6SET2;
+
+public class Test_Matrix1 {
+    public static void main(String[] args){
+
+    }
+}
