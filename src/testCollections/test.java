5b54d199f23ce2a73ab816ae651a24b7b230f52a
Sin0n_aula_mix <emmanuelani1119@gmail.com>
2026-03-02 09:06:05 +0100

    02/03/2026

--- src/testCollections/test.java
+++ src/testCollections/test.java
@@ -1,6 +1,7 @@
 package testCollections;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class test {
 
@@ -15,6 +16,46 @@ public class test {
 		System.out.println(list);
 		
 		System.out.println(list.indexOf("bbb"));
+		
+		list.remove(list.size()-1);
+		
+		System.out.println(list);
+		
+		list.add(2,"xxx");
+		
+		
+		//iterate with index and get
+		
+		String all = "";
+		
+		for(int i = 0 ; i < list.size(); i++) {
+			all = all + list.get(i);
+		}
+		
+		//iterate with iterator // get largest string
+		
+		String maxstr =""; 
+		Iterator<String> it = list.iterator();
+		
+		while(it.hasNext()) {
+			String str = it.next();
+			
+			if(str.length() > maxstr.length()) {
+				maxstr = str;
+			}
+		}
+		
+		
+		// enchanced for
+		
+		for(String Str: list) {
+			
+			if(Str.indexOf("*") != -1 || Str.indexOf("?") != -1) {
+				
+				System.out.println(Str); 
+			}
+			
+		}
 
 	}
 
