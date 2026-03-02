0650ea20fdbe6d06b2bbad42331969726bf94820
Sin0n_aula_mix <emmanuelani1119@gmail.com>
2026-03-02 09:47:59 +0100

    class exampless

--- src/testCollections/Product.java
+++ src/testCollections/Product.java
@@ -1,5 +1,36 @@
 package testCollections;
 
+import java.util.Iterator;
+
 public class Product {
+	private String name;
+	private double price;
+	private int ammount;
+	
+	public Product(String name, Double price, int ammount) {
+
+		this.name = name;
+		this.price = price ;
+		this.ammount = ammount;
+	}
+	
+	
+	public int getAmmount() {
+		return ammount;
+	}
+	public String getName() {
+		return name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return 
+	}
+	
+	
 
 }
--- src/testCollections/ShoppingCart.java
+++ src/testCollections/ShoppingCart.java
@@ -1,5 +1,59 @@
 package testCollections;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ShoppingCart {
+	private String owner;
+	private ArrayList<Product> cart;
+	
+	public ShoppingCart(String owner) {
+		this.owner = owner;
+		this.cart = new ArrayList<Product>();
+		
+	}
+	
+	public void addProduct() {
+		
+	}
+	public double price() {
+		Iterator<Product> it = cart.iterator();
+		while(it.hasNext()) {
+			
+		}
+	}
+	
+	public void removeExpensive(double topPrice) {
+		
+	}
+	
+	
+	public void cleanCart() {//removes elements that its price is negative
+		//for removing while iterating --> use iterators remove method
+		
+		Iterator<Product> it = cart.iterator();
+		while(it.hasNext()) {
+			Product p = it.next();
+			if(p.getAmmount() < 0 || p.getPrice() < 0) {
+				it.remove();//deletes the iterators product and resizes automatically...
+			}
+		}
+		
+	}
+	
+	
+	public void view() {
+		for(Product pr : cart) {
+			System.out.println(pr.toString());
+		}
+	}
+	
+	//to correct.......#
+	public ArrayList<String> boughtProds(){
+		ArrayList<String> names =  new ArrayList<String>();
+		for(String p : names) {
+			p = cart.toString();
+		}
+		return names;
+	}
 
 }
