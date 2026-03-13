da565f9b2ca6806785143427ff91441944162c10
sinon_ <emmanuelani1119@gmail.com>
2026-03-13 10:13:03 +0100

    Work done on 02/03/2026

--- src/empresa/MobilePhone.java
+++ src/empresa/MobilePhone.java
@@ -1,31 +1,33 @@
 package empresa;
 
 public class MobilePhone {
-	private String number;
-	private int battery;
+    private String number;
+    private int battery;
 
-	public MobilePhone(String number, int battery) {
-		this.battery = battery;
-		this.number = number;
-	}
 
-	public void use() {
-		if (battery > 0) {
-			battery -= 1;
-		} else {
-			System.out.println("Phone is dead, Please charge....");
-		}
-	}
+    public MobilePhone(String number, int battery){
+        this.battery = battery;
+        this.number = number;
+    }
 
-	public String getNumber() {
-		return number;
-	}
+    public void use(){
+        if(battery > 0 ){
+            battery -= 1;
+        }
+        else{
+            System.out.println("Phone is dead, Please charge....");
+        }
+    }
 
-	public int getBattery() {
-		return battery;
-	}
+    public String getNumber() {
+        return number;
+    }
 
-	public void setNumber(String number) {
-		this.number = number;
-	}
+    public int getBattery() {
+        return battery;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
--- src/empresa/Person.java
+++ src/empresa/Person.java
@@ -1,49 +1,50 @@
 package empresa;
 
 public class Person {
-	private String name, dni;
-	private int height, weight;
+    private String name, dni;
+    private int height, weight;
 
-	public Person(String dni, String name, int height, int weight) {
-		this.dni = dni;
-		this.weight = weight;
-		this.name = name;
-		this.height = height;
-	}
+    public Person(String dni, String name, int height, int weight){
+        this.dni = dni;
+        this.weight = weight;
+        this.name = name;
+        this.height = height;
+    }
 
-	public Person(String dni) {
-		this.dni = dni;
-		this.height = 0;
-		this.weight = 0;
-		this.name = "";
-	}
+    public Person(String dni){
+        this.dni = dni;
+        this.height = 0;
+        this.weight = 0;
+        this.name = "";
+    }
 
-	public int getHeight() {
-		return height;
-	}
 
-	public int getWeight() {
-		return weight;
-	}
+    public int getHeight() {
+        return height;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public int getWeight() {
+        return weight;
+    }
 
-	public void setHeight(int height) {
-		this.height = height;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setHeight(int height) {
+        this.height = height;
+    }
 
-	public void setWeight(int weight) {
-		this.weight = weight;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	@Override
-	public String toString() {
-		return "name: " + this.name + " weight: " + this.weight;
-	}
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+   public String  toString(){
+        return "name: " + this.name + " weight: " + this.weight;
+    }
 }
