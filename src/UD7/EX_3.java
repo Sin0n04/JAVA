0dc8bf19c7b056075871a8ae8d8f55f8d49db082
Sin0n04 <emmanuelani1119@gmail.com>
2026-04-30 13:34:39 +0200

    work done on 30/04/2026 -

--- src/UD7/EX_3.java
+++ src/UD7/EX_3.java
@@ -26,27 +26,56 @@ public class EX_3 {
     public static void compress(String orgfile,String compressedfile){
         try(BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(compressedfile));
             BufferedInputStream bis = new BufferedInputStream(new FileInputStream(orgfile))){
-            while(true){
-                try {
-                    byte prevalue = (byte) bis.read();
-                    byte value = (byte) bis.read();
-                    int reps = 0;
-                    while (value == prevalue) {
-                        prevalue = (byte) bis.read();
-                        value = (byte) bis.read();
-                        reps++;
-                    }
-                    bos.write(prevalue + reps);
-                }
-                catch(EOFException e ){
-                    break;
-                }
+           int current = bis.read();
 
-            }
+           if(current == -1){
+               return;
+           }
+
+           int count = 1;
+           int next;
+
+           while((next= bis.read())!= -1){
+               if(next == current && count < 255){
+                   count++;
+               }
+               else{
+                   bos.write(current);
+                   bos.write(count);
+
+                   current = next;
+                   count = 1;
+               }
+           }
+
+           bos.write(current);
+           bos.write(count);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
+    }
+
+    public static void decompress(String compressedfile, String decompressedfile){
+        try(BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(decompressedfile));
+            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(compressedfile))){
+            int value;
+            while ((value = bis.read()) != - 1){
+               int reps = bis.read();
+
+               if (reps == -1){
+                   throw new IOException("corrupted file, missing repetition byte");
+               }
+
+                for (int i = 0; i < reps; i++) {
+                    bos.write(value);
+                }
+            }
+
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
